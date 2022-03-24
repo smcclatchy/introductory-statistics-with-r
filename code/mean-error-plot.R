a3b4e34d383fc56d286f899a514e68c62f35f0f3
Sue McClatchy <susan.mcclatchy@jax.org>
2022-03-24 08:22:33 -0400

    edit figures

--- code/mean-error-plot.R
+++ code/mean-error-plot.R
@@ -12,6 +12,7 @@ mean_error_plot <- ggplot(groupMeans, aes(x = Group, y = avg_size)) +
   geom_errorbar(aes(ymin=avg_size-std_dev, ymax=avg_size+std_dev),
                 size=.3, width=.2, color = "gray") +
   ylab("Mean tumor size") +
+  theme_minimal() +
   ggtitle("Mean tumor size and standard deviations of four experimental groups")
 
 png("../fig/mean-error-plot.png")
